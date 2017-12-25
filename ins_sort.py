0221b07822c61d3cf6e7ee3208e02c8faa23afab
asherpriya <priyaasher29@gmail.com>
2017-12-25 14:49:23 -0800

    Add insertion sort code version 1

--- ins_sort.py
+++ ins_sort.py
@@ -1,1 +1,21 @@
-hi
+import random
+
+def ins_sort(ip_arr):
+   l=len(ip_arr)
+   for j in range(1,l):
+     nxt=ip_arr[j]
+     i=j-1
+     while(i>=0 and ip_arr[i]>nxt):
+            ip_arr[i+1]=ip_arr[i]
+            i-=1
+     ip_arr[i+1]=nxt
+   return ip_arr
+
+l = random.randint(5, 100)
+arr = []
+for i in range(l):
+	arr.append(random.randint(0, 100))
+
+print 'Original Array', arr
+arr = ins_sort(arr)
+print 'Sorted Array', arr
